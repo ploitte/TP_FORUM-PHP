d2db6f225e5347f6c2e808442d8cf4af5a6fb947
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-09 12:27:25 +0200

    Commentaires: index.php et models/fonction.php

--- index.php
+++ index.php
@@ -11,11 +11,23 @@
 
     switch($page){
 
-    case "accueil":
-
+        case "accueil":
+    /* Alfonso:bien joué pour les variables dans les requêtes
+     * par contre tu as mis des nom erronés le paramètre nom est en fait un champ
+     * le paramètre bdd n'est pas un base de données mais une table
+     * fait attention quand tu regardes les tutos sur internet
+     */
     $cat = takeCat("nom", "f_categories");
+    /*
+     * Alfonso:Chaque view est sensé être dans la page qui lui correspond
+     * dans notre cas dans le case "acceuile de notre switch et donc la ligne
+     * include("views/Accueil.php");
+     * doit être ici mais tu l'auras vu quand tu rajouteras d'autres pages
+     * */
     break;
     }
+    /* Alfonso: merci pour le fichier SQL */
+
 
   include("views/Accueil.php");
 ?>
--- models/fonction.php
+++ models/fonction.php
@@ -1,10 +1,11 @@
 <?php
 
     function connectToBdd(){
-        $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=UTF8','root','root');
+        $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=UTF8','root','');
         return $bdd;
     }
 
+    /* voir commentaire dans index.php concernant cette fonction */
     function takeCat($table, $bdd){
         $connect = connectToBdd();
         $pdo = $connect -> prepare("SELECT $table FROM $bdd");
